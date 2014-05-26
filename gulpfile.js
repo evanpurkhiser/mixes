ebdc5d00b0e6e3ed5b2bf54a0d8442d8ddf20984
Evan Purkhiser <evanpurkhiser@gmail.com>
2014-05-26 15:52:54 -0400

    Add missing semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var gulp    = require('gulp'),
     sass    = require('gulp-sass'),
@@ -10,22 +10,24 @@ var buildDir = 'build';
 gulp.task('sass', function() {
     return gulp.src('src/sass/screen.sass')
         .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
-        .pipe(gulp.dest(buildDir))
+        .pipe(gulp.dest(buildDir));
 });
 
 gulp.task('js', function() {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('app.js'))
-        .pipe(gulp.dest(buildDir))
+        .pipe(gulp.dest(buildDir));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/sass/**/*.sass', ['sass'])
-    gulp.watch('src/js/**/*.js', ['js'])
+    gulp.watch('src/index.html', ['index']);
+    gulp.watch('src/sass/**/*.sass', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+
 });
 
 gulp.task('serve', function() {
-    connect.server({root: buildDir})
+    connect.server({root: buildDir});
 });
 
-gulp.task('default', ['serve', 'watch'])
+gulp.task('default', ['serve', 'watch']);
