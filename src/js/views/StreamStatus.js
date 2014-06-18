060c487627864b4ed8b24220033e27fdbfa77757
Evan Purkhiser <evanpurkhiser@gmail.com>
2014-06-18 04:03:28 -0400

    Add clock for when the stream started

--- src/js/views/StreamStatus.js
+++ src/js/views/StreamStatus.js
@@ -2,6 +2,7 @@
 
 var Backbone   = require('backbone'),
     underscore = require('underscore'),
+    moment     = require('moment'),
     template   = require('../../templates/stream_status.mustache');
 
 module.exports = Backbone.View.extend(
@@ -16,6 +17,32 @@ module.exports = Backbone.View.extend(
         this.listenTo(this.model, 'change', this.render);
     },
 
+    /**
+     * Handle updating the 'started streaming' statistic every so often
+     */
+    watchStartedTime: function()
+    {
+        var start_date = moment(new Date(this.model.get('stream_start'))),
+            element    = this.$('.start-time'),
+            startWatch = function()
+            {
+                element.text(start_date.fromNow());
+            };
+
+        startWatch();
+
+        if (this.startedTimer === undefined)
+        {
+            this.startedTimer = setInterval(startWatch, 5000);
+        }
+
+        return this;
+    },
+
+    /**
+     * Called just after the JS event loop ticks over after rendering. Primarily
+     * useful for when we need to
+     */
     postRender: function()
     {
         this.$el.toggleClass('live', this.model.get('active'));
@@ -28,6 +55,8 @@ module.exports = Backbone.View.extend(
             this.$('.genre').text(this.model.get('genre'));
             this.$('.listeners').text(this.model.get('listeners'));
             this.$('.listener-peak').text(this.model.get('listener_peak'));
+
+            this.watchStartedTime();
         }
 
         underscore.defer(underscore.bind(this.postRender, this));
